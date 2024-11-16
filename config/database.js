1f9ee10bb5cdfb28bac3d9c9074e004b26b7881a
Shekhar Singh <shekharkrsingh6432@gmail.com>
2024-11-16 11:49:05 +0530

    Mongodb conifg update

--- config/database.js
+++ config/database.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-exports.connect = async () => {
-    try {
-        // Connect to MongoDB using the URL from environment variables
-        await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("DB connected successfully");
-    } catch (error) {
-        console.error("Issue in DB Connection:", error.message);
-        process.exit(1); // Exit process with failure status
-    }
+exports.connect = () => {
+    mongoose.connect( process.env.MONGODB_URL)
+    .then(() => console.log("DB connected successfully"))
+    .catch( (error) => {
+        console.log('Issue in DB Connection');
+        console.error(error);
+        process.exit(1);
+    })
 };
